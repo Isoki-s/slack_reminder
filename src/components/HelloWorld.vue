33ed0600d486d85fc77722ceed41632f1235fc2e
Satoru Isoki <isokisatoru@aol.com>
2018-08-02 01:42:23 +0900

    Vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,39 +1,19 @@
-<template lang="pug">
-  .hello
-    h1 {{ msg }}
-    p For guide and recipes on how to configure / customize this project,
-      br check out the
-      a(href="https://cli.vuejs.org" target="_blank") vue-cli documentation
-    h3 Installed CLI Plugins
-    ul
-      li
-        a(href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank")
-      li
-        a(href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank")
-    h3 Essential Links
-    ul
-      li
-        a(href="https://vuejs.org" target="_blank") Core Docs
-      li
-        a(href="https://forum.vuejs.org" target="_blank") Forum
-      li
-        a(href="https://chat.vuejs.org" target="_blank") Community Chat
-      li
-        a(href="https://twitter.com/vuejs" target="_blank") Twitter
-      li
-        a(href="https://news.vuejs.org" target="_blank") News
-    h3 Ecosystem
-    ul
-      li
-        a(href="https://router.vuejs.org" target="_blank") vue-router
-      li
-        a(href="https://vuex.vuejs.org" target="_blank") vuex
-      li
-        a(href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank") vue-devtools
-      li
-        a(href="https://vue-loader.vuejs.org" target="_blank") vue-loader
-      li
-        a(href="https://github.com/vuejs/awesome-vue" target="_blank") awesome-vue
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
 
 <script>
@@ -45,19 +25,21 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
