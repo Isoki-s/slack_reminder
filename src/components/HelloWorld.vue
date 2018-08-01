f2d70a6a6579b1e25b29963b2b5c61e125b1be5a
Satoru Isoki <isokisatoru@aol.com>
2018-08-02 01:51:03 +0900

    pug stylus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,12 @@
-<template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+<template lang="pug">
+  v-container(fluid)
+    v-slide-y-transition(mode="out-in")
+      v-layout(column align-center)
+        img(src="@/assets/logo.png" alt="Vuetify.js" class="mb-5")
+        blockquote &#8220;First, solve the problem. Then, write the code.&#8221;
+          footer
+            small
+              em &mdash;John Johnson
 </template>
 
 <script>
@@ -27,19 +20,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="stylus">
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+
 </style>
