3eb44203d5317fc686f4f0fce9694ffe17ecca0d
Satoru Isoki <isokisatoru@aol.com>
2018-08-03 14:46:28 +0900

    form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,15 @@
   v-container(fluid)
     v-slide-y-transition(mode="out-in")
       v-layout(column align-center)
+        v-text-field(
+          v-model="name"
+          :error-messages="nameErrors"
+          :counter="10"
+          label="Name"
+          required
+          @input="$v.name.$touch()"
+          @blur="$v.name.$touch()"
+        )
         v-menu(
           ref="menu"
           :close-on-content-click="false"
@@ -25,6 +34,9 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, maxLength } from 'vuelidate/lib/validators'
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -35,8 +47,35 @@ export default {
       date: null,
       menu: false,
       modal: false,
+      name: '',
     }
-  }
+  },
+
+  mixins: [validationMixin],
+
+    validations: {
+      name: { required, maxLength: maxLength(100) },
+    },
+
+    computed: {
+      nameErrors () {
+        const errors = []
+        if (!this.$v.name.$dirty) return errors
+        !this.$v.name.maxLength && errors.push('名前長すぎやろ')
+        !this.$v.name.required && errors.push('いや、誰に通知すんねん')
+        return errors
+      }
+    },
+
+    methods: {
+      submit () {
+        this.$v.$touch()
+      },
+      clear () {
+        this.$v.$reset()
+        this.name = ''
+      }
+    }
 }
 </script>
 
